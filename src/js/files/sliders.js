fd283afcdddc66db21f5d33b61a67f27b87c53d8
Denis <95119609+Dtulumbasov28@users.noreply.github.com>
2023-03-14 17:35:29 +0300

    Сверстана страница Лечение алкоголизма, добавлена анимация на некоторые ссылки, внесены правки по коду

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -367,6 +367,39 @@ function initSliders() {
     // События
     on: {},
   });
+  var spollersAlcoSlider = new Swiper(".spollers-alcoholism__slider", {
+    modules: [Navigation],
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 12,
+    speed: 500,
+
+    // Кнопки "влево/вправо"
+    navigation: {
+      prevEl: ".spollers-alcoholism__button-prev",
+      nextEl: ".spollers-alcoholism__button-next",
+    },
+
+    // Брейкпоинты
+
+    breakpoints: {
+      1: {
+        slidesPerView: 1,
+      },
+      658: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1440: {
+        slidesPerView: 4,
+      },
+    },
+
+    // События
+    on: {},
+  });
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
